c4058c8281b72484476a603e6c059ffdc2088f16
Ana Ortez <anaortez23@gmail.com>
2023-02-02 09:24:51 -0500

    which-number-is-greater

---
+++ else-if-python/number-negative-or-positive.py
@@ -0,0 +1,13 @@
+
+number = float(input('Enter your number:'))
+
+if number > 0:
+    print('Number input is positive')
+
+elif number == 0:
+    print('Number is 0, nor positive, nor negative')
+
+else:
+    print('Number is negative')
+
+    # hello there
