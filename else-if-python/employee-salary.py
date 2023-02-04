c2ca11af0b533479668f62be4c85a29ead0d191b
Ana Ortez <anaortez23@gmail.com>
2023-02-04 09:56:50 -0500

    double if condition

--- else-if-python/employee-salary.py
+++ else-if-python/employee-salary.py
@@ -1,16 +1,3 @@
-"""
-salary = int(input('Enter your salary: '))
-yearOfEmp = int(input('Enter number of year: '))
-
-if salary >= 30000:
-    if yearOfEmp >= 2:
-        print('You are qualified')
-    else:
-        print('You are not working long to be qualified')
-else:
-    print('Not qualified because you did not meet salary req.')
-
-"""
 
 salary = int(input('Enter your salary: '))
 yearOfEmp = int(input('Enter number of year: '))
