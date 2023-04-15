2a79d3e931a79f50311b616a1b20ed8ba560f185
Ana Ortez <anaortez23@gmail.com>
2023-04-15 07:22:11 -0400

    from module to function

---
+++ module/module-to-func.py
@@ -0,0 +1,12 @@
+def main():
+    length = int(input("Enter length: "))
+    width = int(input("Enter width: "))
+    calcArea(length, width)
+
+
+def calcArea(l, w):
+    result = l*w
+    print(result)
+
+
+main()
